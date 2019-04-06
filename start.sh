cb01b2b4a6af4e9134fc17b8d1f82de8d220a043
Vien Dinh <maxvien@gmail.com>
2019-04-06 16:18:19 +0700

    Remove docker rm and docker stop

--- start.sh
+++ start.sh
@@ -1,11 +1,8 @@
 containerName=$(basename $PWD)
 
-docker stop $containerName
-docker rm $containerName
-
 docker run --name $containerName -p 8080:80 -d \
-    -v $(pwd)/modules:/var/www/html/modules \
-    -v $(pwd)/profiles:/var/www/html/profiles \
-    -v $(pwd)/sites:/var/www/html/sites \
-    -v $(pwd)/themes:/var/www/html/themes \
-    drupal
+  -v $(pwd)/modules:/var/www/html/modules \
+  -v $(pwd)/profiles:/var/www/html/profiles \
+  -v $(pwd)/sites:/var/www/html/sites \
+  -v $(pwd)/themes:/var/www/html/themes \
+  drupal
